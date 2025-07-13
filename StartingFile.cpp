3f38645617bf58823c4fd5dd5dd29585db6f44d4
Rudra-Prasad-Das <rudrarpd08@gmail.com>
2025-07-13 16:06:00 +0530

    Added the initial file

---
+++ StartingFile.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Journal{
+    public:
+    string title;
+    vector<string> entries;
+   
+    Journal(const string &title){
+        this->title = title;
+    }
+
+    void add(const string& entry){
+        static int count = 0 ;
+        entries.push_back("Entry no" + to_string(count) + ":" + entry);
+        count++;
+    }
+
+    void display(){
+        for(auto entry : entries){
+            cout<<entry<<"\n";
+        }
+    }
+
+};
+int main(){
+    Journal* obj = new Journal("Dear Diary");
+
+    obj->add("Today is Sunday");
+    obj->add("Today is Monday");
+    obj->display();
+
+    return 0;
+}
+
+/**
+ * @brief How to compile and run cpp code in terminal
+ * g++ <filename>.cpp -o <filename>.exe
+ * <filename>.exe
+ */
