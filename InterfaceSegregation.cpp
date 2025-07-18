2f4127b88e102261c93e37154997ec7a7c587667
Rudra-Prasad-Das <rudrarpd08@gmail.com>
2025-07-18 19:35:13 +0530

    Added InterfaceSegregation.cpp:

---
+++ InterfaceSegregation.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+class Document{
+    public:
+    Document(){
+
+    }
+};
+class IMachine{
+    virtual void print(Document& doc) = 0;
+    virtual void scan(Document& doc) = 0;
+    virtual void fax(Document& doc) = 0;
+};
+
+class MFP:public IMachine{
+    void print(Document& doc){
+
+    }
+    void scan(Document& doc){
+
+    }
+
+    void fac(Document& doc){
+
+    }
+};
+
+class Scanner:public IMachine{
+    void print(Document& doc){
+        // throw some Exception 
+        // or let them be empty
+        // Sending the client wrong message is giving a wrong API
+    }
+    void scan(Document& doc){
+        // Only functionality required
+    }
+
+    void fac(Document& doc){
+
+    }
+};
+
